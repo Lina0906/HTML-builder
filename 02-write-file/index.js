7531d62deeee9e26a74f59727a2c0a3952dfe43a
Lina Churbanova <lina.moiseeva@gmail.com>
2024-01-22 18:16:47 +0400

    feat: add 02-write-file task solution

---
+++ 02-write-file/index.js
@@ -0,0 +1,17 @@
+const fs = require("fs");
+const path = require("path");
+
+const { stdin, stdout } = process;
+const output = fs.createWriteStream(path.join(__dirname, 'text.txt'), 'utf-8');
+stdout.write("Please, enter text\n");
+stdin.on("data", (data) => {
+const text = data.toString();
+if (text.toLowerCase().includes(".exit")) {
+  process.exit();
+} 
+output.write(`${text}`);
+});
+process.on('SIGINT', () => {  
+  process.exit();
+});
+process.on("exit", () =>  stdout.write("Goodbye!"));
